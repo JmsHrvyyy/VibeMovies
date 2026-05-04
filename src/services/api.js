e7b9473cdf6ab1ab7653ec1a04fc351ec48a8fb4
JmsHrvyyy <jamesharveyaustria3@gmail.com>
2026-05-04 16:14:31 +0800

    feat: implement movie search functionality and add API service

---
+++ src/services/api.js
@@ -0,0 +1,29 @@
+// services/api.js
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+// Function para sa Trending Movies (Yung ginawa natin kanina)
+export const getTrendingMovies = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    return [];
+  }
+};
+
+// // Function for Searching Movies
+export const searchMovies = async (query) => {
+  try {
+    // Ginagamit ang 'search/movie' endpoint at pinapasa ang 'query' (yung tinype ng user)
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error searching movies:", error);
+    return [];
+  }
+};
